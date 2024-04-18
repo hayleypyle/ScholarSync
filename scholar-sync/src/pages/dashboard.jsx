b5b324e191497d197839091a695cc5e9e5fc842a
hayleypyle <hstowell@icloud.com>
2024-04-18 11:54:11 -0400

    updated dashboard page to have a logo and links to github and linkedin

--- scholar-sync/src/pages/dashboard.jsx
+++ scholar-sync/src/pages/dashboard.jsx
@@ -6,6 +6,9 @@ import './css/dashboard.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import SidebarMenu from './SidebarMenu'
+import * as Icon from 'react-bootstrap-icons';
+import image from '../images/AppleDesign.jpg'
+
 
 export default function Dashboard() {
     
@@ -26,8 +29,22 @@ export default function Dashboard() {
             </div>
 
             <div className="col-lg-9">
-    <div className="question-display">
-    <h2>Welcome to Scholar Sync!</h2>
+    <div className="welcome-container">
+        <div className="welcome">
+        <h2>Welcome to Scholar Sync!</h2>
+        <h5> Scholar Sync is a space where educators can seek out the advice and 
+            expertise of other teachers</h5>
+        <h6>Select a topic to get started</h6>
+        <img src={image} className="image"></img>
+        </div>
+        <div className="icon-container">
+        <a href="https://github.com/hayleypyle">
+        <Icon.Github className="icon"/>
+        </a>
+        <a href="https://www.linkedin.com/in/hayley-pyle/">
+        <Icon.Linkedin className="icon"/>
+        </a>
+        </div>
     </div>
     </div>
     </div>
